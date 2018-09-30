62627527ef63e4189c854aa20722ac1e799fbb60
khai96_ <hvksmr1996@gmail.com>
2018-09-30 14:42:24 +0700

    Create install script

---
+++ install.js
@@ -0,0 +1,18 @@
+'use strict'
+const process = require('process')
+const config = require('cosmiconfig')('require-external-programs')
+const check = require('./index')
+
+check(config).then(
+  () => process.exit(0),
+  error => {
+    if (error instanceof check.NotFoundError) {
+      console.error('[ERROR] One or more required programs is missing')
+      error.list.forEach(name => console.info(`- Missing ${name}`))
+      process.exit(1)
+    } else {
+      console.error(error)
+      process.exit(-1)
+    }
+  }
+)
